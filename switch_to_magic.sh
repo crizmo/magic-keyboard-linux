4995770478e158efd738f47bacce367f78f1ffc7
Kurizu <83665497+crizmo@users.noreply.github.com>
2025-02-05 15:09:05 +0530

    🔥 notification

--- switch_to_magic.sh
+++ switch_to_magic.sh
@@ -7,3 +7,6 @@ xmodmap ~/.Xmodmap
 
 # Enable the Fn key behavior for the Magic Keyboard (Function keys without pressing Fn)
 echo 2 | sudo tee /sys/module/hid_apple/parameters/fnmode
+
+# Show a notification with increased display time (e.g., 5000 milliseconds = 5 seconds)
+notify-send -t 1500 "Keyboard Layout" "Switched to Magic Keyboard Layout"
