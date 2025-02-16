6a5d8d6298a1939c89f2bcfd7fb4625f2acde3d2
Kurizu <83665497+crizmo@users.noreply.github.com>
2025-02-16 04:51:11 -0500

    finally X11 moment

--- switch_to_basic.sh
+++ switch_to_basic.sh
@@ -3,7 +3,6 @@
 setxkbmap -layout us
 
 # Reset the Fn key behavior for the laptop's built-in keyboard
-echo 0 | pkexec tee /sys/module/hid_apple/parameters/fnmode
-
+echo 0 | sudo tee /sys/module/hid_apple/parameters/fnmode
 # Show a notification
 notify-send -t 1500 "Keyboard Layout" "Switched to Basic Keyboard Layout"
